8e9c85706db9e430fbed0a55560a67fb653039ad
Stanley Shyiko <stanley.shyiko@gmail.com>
2017-09-19 01:50:01 -0700

    jabba@0.8.0 sync

--- developer-ibm-com-javasdk.js
+++ developer-ibm-com-javasdk.js
@@ -6,7 +6,109 @@ process.on('unhandledRejection', (reason, promise) => {
   process.exit(1)
 })
 
-fetch('https://developer.ibm.com/javasdk/downloads/')
+const latestRef = process.argv[2] === 'true'
+
+function list(url) {
+  return fetch(url)
+    .then((res) => {
+      if (!res.ok) { throw new Error(url + ' ' + res.status) }
+      return res
+    })
+    .then((res) => res.text())
+    .then((html) => {
+      const $ = cheerio.load(html)
+      const links = []
+      $('img[alt="[DIR]"],[alt="[   ]"]').each(function () {
+        const href = $(this).next().attr('href')
+        href && links.push(href)
+      })
+      return {
+        url,
+        links
+      }
+    })
+}
+
+list('http://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/')
+  .then(({links}) => {
+    const cache = {}
+    return links.reduce((p, r) => {
+      const m = r.match(/(\d+.\d+.\d+.\d+)\//)
+      if (m) {
+        const ref = m[1]
+/*
+        if (ref !== '8.0.3.22') {
+          return p
+        }
+*/
+        const split = ref.split('.')
+        const versionPrefix = `1.${split[0]}.${split[1]}`
+        const version = `${versionPrefix}-${split[2]}.${split[3]}`
+        const pushVersionPrefix = latestRef && !cache[versionPrefix]
+        cache[versionPrefix] = true
+        return p
+          .then((state) =>
+            list(`http://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/${ref}/linux/x86_64/`)
+              .catch(() => list(`http://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/${ref}/linux/x64/`))
+              .then(({url, links}) => {
+                const ll = links.filter((v) => v.startsWith('ibm-java-sdk-'))
+                if (ll.length != 1) {
+                  console.error("Unexpected content of " + url)
+                  process.exit(1)
+                }
+                let r = state
+                if (pushVersionPrefix) {
+                  r = r.concat({
+                    os: 'linux',
+                    arch: 'amd64',
+                    version: versionPrefix,
+                    url: url + ll[0]
+                  })
+                }
+                return r.concat({
+                  os: 'linux',
+                  arch: 'amd64',
+                  version,
+                  url: url + ll[0]
+                })
+              })
+          )
+          .then((state) =>
+            list(`http://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/${ref}/linux/i386/`)
+              .catch(() => list(`http://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/${ref}/linux/x86/`))
+              .then(({url, links}) => {
+                const ll = links.filter((v) => v.startsWith('ibm-java-sdk-'))
+                if (ll.length != 1) {
+                  console.error("Unexpected content of " + url)
+                  process.exit(1)
+                }
+                let r = state
+                if (pushVersionPrefix) {
+                  r = r.concat({
+                    os: 'linux',
+                    arch: '386',
+                    version: versionPrefix,
+                    url: url + ll[0]
+                  })
+                }
+                return r.concat({
+                  os: 'linux',
+                  arch: '386',
+                  version,
+                  url: url + ll[0]
+                })
+              })
+          )
+      }
+      return p
+    }, Promise.resolve([]))
+  })
+  .then((ee) => {
+    console.log(JSON.stringify(ee, null, '  '))
+  })
+
+/*
+fetch('https://developer.ibm.com/javasdk/downloads/sdk8/')
   .then((res) => {
     if (!res.ok) { throw new Error('' + res.status) }
     return res
@@ -32,4 +134,4 @@ fetch('https://developer.ibm.com/javasdk/downloads/')
     })
     console.log(JSON.stringify(ee, null, '  '))
   })
-
+*/
--- index.js
+++ index.js
@@ -17,8 +17,8 @@ stdin.pipe(concat((data) => {
   next(JSON.parse(data)).then(
     (updatedData) => {
       console.log(stringify(updatedData, {
-        cmp: (l, r) => semver.valid(l.key) && semver.valid(r.key)
-          ? semver.compare(r.key, l.key) : l.key.localeCompare(r.key),
+        cmp: (l, r) => semver.valid(l.key, true) && semver.valid(r.key, true)
+          ? semver.compare(r.key, l.key, true) : l.key.localeCompare(r.key),
         space: 2
       }))
     },
@@ -35,7 +35,7 @@ function next(data) {
     if (map) {
       o[key] = Object.keys(map).reduce((acc, k) => {
         const v = map[k]
-        if (!~v.indexOf('oracle.com') && !~v.indexOf('java.net')) {
+        if (!~v.indexOf('oracle.com') && !~v.indexOf('java.net') && !~v.indexOf('ibm.com') && !~v.indexOf('zulu-pre')) {
           acc[k] = v
         }
         return acc
@@ -47,6 +47,8 @@ function next(data) {
     Object.keys(byArch).forEach((arch) => {
       truncate(byArch[arch], 'jdk')
       truncate(byArch[arch], 'jdk@sjre')
+      truncate(byArch[arch], 'jdk@ibm')
+      truncate(byArch[arch], 'jdk@zulu')
     })
   })
   return Promise.all([
@@ -64,7 +66,14 @@ function next(data) {
 */
     node('jdk9-java-net.js').then((data) => ({ns: 'jdk', data})),
     node('zulu-org.js').then((data) => ({ns: 'jdk@zulu', data})),
-    node('developer-ibm-com-javasdk.js').then((data) => ({ns: 'jdk@ibm', data}))
+    node('zulu-org-eap.js').then((data) => ({ns: 'jdk@zulu', data})),
+    node('developer-ibm-com-javasdk.js true').then((data) => ({ns: 'jdk@ibm', data})),
+    node('adoptopenjdk-net.js https://api.adoptopenjdk.net/openjdk8/releases').then((data) => ({ns: 'jdk@adopt', data})),
+    node('adoptopenjdk-net.js https://api.adoptopenjdk.net/openjdk9/releases').then((data) => ({ns: 'jdk@adopt', data})),
+    node('adoptopenjdk-net.js https://api.adoptopenjdk.net/openjdk9-openj9/releases').then((data) => ({ns: 'jdk@adopt-openj9', data})),
+/*
+    node('adoptopenjdk-net.js http://localhost:8000/adoptopenjdk-openjdk8.json').then((data) => ({ns: 'jdk@adopt', data})),
+*/
   ]).then((nn) => {
     nn.forEach((n) => {
       n.data.forEach((e) => {
