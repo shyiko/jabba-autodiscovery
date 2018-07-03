5724024a066f915a7e2e21da67dabe70b1cb12f8
Martynas Mickevičius <self@2m.lt>
2018-07-03 17:53:37 +0300

    Add Graal RC3

--- src/github-com-oracle-graal.js
+++ src/github-com-oracle-graal.js
@@ -12,6 +12,12 @@ console.log(
       "arch": "amd64",
       "version": "1.0.0-rc2",
       "url": "https://github.com/oracle/graal/releases/download/vm-1.0.0-rc2/graalvm-ce-1.0.0-rc2-linux-amd64.tar.gz"
+    },
+    {
+      "os": "linux",
+      "arch": "amd64",
+      "version": "1.0.0-rc3",
+      "url": "https://github.com/oracle/graal/releases/download/vm-1.0.0-rc3/graalvm-ce-1.0.0-rc3-linux-amd64.tar.gz"
     }
   ], null, '  ')
 )
