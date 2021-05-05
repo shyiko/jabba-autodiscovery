2b098d336aec085885fcc28747463bbbae3706ea
Will Cohen <willcohen@users.noreply.github.com>
2021-05-05 09:51:15 -0400

    Exclude espresso from graal

--- src/github-com-graalvm.js
+++ src/github-com-graalvm.js
@@ -21,7 +21,7 @@ const fetch = require('node-fetch')
       }
       for (const asset of release.assets || []) {
         const url = asset.browser_download_url
-        if (url.endsWith('.jar') || url.endsWith('.sha256')) {
+        if (url.endsWith('.jar') || url.endsWith('.sha256') || url.includes('espresso')) {
           console.error(`skip(ext): ${url}`)
           continue
         }
